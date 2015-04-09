d686aa7de5c5943818996235812ce0fb65a6b515
ahmad_darweesh <ahmad_darweesh@ca-adarweesh.SiliconEgypt.com>
2015-04-09 11:06:23 +0200

    Reamaining entities

--- inventory.pl/src/main/java/inventory/pl/entities/Procurement.java
+++ inventory.pl/src/main/java/inventory/pl/entities/Procurement.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -39,7 +41,8 @@ public class Procurement implements Serializable {
     @OneToOne
     @JoinColumn(name = "request_id")
     private NeedsRequest request;
-
+@OneToMany(cascade=CascadeType.ALL,mappedBy="roder")
+List<OrderDetails>details;
     public long getId() {
         return id;
     }

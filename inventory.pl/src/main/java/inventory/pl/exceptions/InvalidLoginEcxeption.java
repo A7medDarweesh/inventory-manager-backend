c1e1153ab3313c5630e3dba9c860063d34a6a0d6
ahmed_darweeesh <ahmed_darweeesh@pc>
2015-06-12 15:06:10 +0200

    spring concurrent execution

---
+++ inventory.pl/src/main/java/inventory/pl/exceptions/InvalidLoginEcxeption.java
@@ -0,0 +1,10 @@
+package inventory.pl.exceptions;
+
+public class InvalidLoginEcxeption extends Exception {
+
+	public InvalidLoginEcxeption(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
